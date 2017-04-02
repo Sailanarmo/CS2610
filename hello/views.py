a41333601adf3c5b04bf0de381bef4b758cae358
Raul Ramirez <rlramirez12@gmail.com>
2017-04-02 05:04:50 +0000

    Pushing workspace up to github

---
+++ hello/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import time
+import pytz
+
+from datetime import date
+from datetime import datetime
+
+# Create your views here.
+
+timeZone = pytz.timezone('US/Mountain')
+
+currDate = date.today()
+currTime = datetime.now(timeZone)
+
+
+def index(request):
+    return HttpResponse("Hello World "+ "</br>" + "The date is: " + currDate.strftime('%b %d, %Y')
+    + ", The time is: " + currTime.strftime('%I:%M %p') + "</br>")
